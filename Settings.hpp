fa8a94f3690f817f6a87809ea0d3654cfd427fc6
Rudy Clark <rudy.uzb@gmail.com>
2024-09-02 12:55:31 +0500

    added settings to control boids vision radius, and font size for text renderer

--- Settings.hpp
+++ Settings.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
 namespace Settings {
-	constexpr int screenWidth{ 1920 };
-	constexpr int screenHeight{ 1080 };
+	constexpr int screenWidth{ 1280 };
+	constexpr int screenHeight{ 720 };
 	constexpr char title[]{ "Boids Simulation" };
 	constexpr int FPS{ 75 };
 	constexpr int TotalBoids{ 300 };
-	constexpr int perceptionRadius{ 40 };
+	constexpr int perceptionRadius{ 50 };
+	constexpr int fontSize{ 16 };
 }
